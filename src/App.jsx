45378583a67fb28c176cfe5a59848f57d3ab0fee
yujhenchen <yujhen.chen93@gmail.com>
2024-01-01 21:36:10 +0800

    change file type, add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./styles.css";
+import Header from "./Header";
 
 export default function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
