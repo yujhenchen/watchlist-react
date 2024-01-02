3d9a4cb2e320760d9c5f32594d62d98805b71f70
yujhenchen <yujhen.chen93@gmail.com>
2024-01-02 20:17:22 +0800

    add react router, style component add component files

---
+++ src/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  Link } from "react-router-dom";
+
+export default function Navigation() {
+  return (
+    <nav>
+      <li>
+      <Link to="/">Home</Link>
+      </li>
+      <li>
+      <Link to="/">Home</Link>
+      </li>
+    </nav>
+  )
+}
---
+++ src/mywatchlist-page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function MyWatchListPage() {
+  return (
+    <div>mywatchlist-page</div>
+  )
+}
